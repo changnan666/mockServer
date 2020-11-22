7903c72d089b4940716854065141d3d9965c79d8
lichangnan <changnan666@gmail.com>
2020-11-22 21:13:06 +0800

    基础页面完成

---
+++ src/App/data.ts
@@ -0,0 +1,79 @@
+export const data = [
+  {
+    projectName: "项目名称1",
+    baseUrl: "/base1",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+  {
+    projectName: "项目名称2",
+    baseUrl: "/base2",
+    paths: [
+      {
+        name: "路径",
+        path: "/demo",
+        data: "1",
+      },
+    ],
+  },
+];
